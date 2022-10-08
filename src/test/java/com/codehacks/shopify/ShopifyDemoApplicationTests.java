8c548aae0d3491191c7e6ee4a748423ff0c03d7c
Rume7 <electengrhume@gmail.com>
2022-10-08 08:44:22 +0100

    Added source packages.

---
+++ src/test/java/com/codehacks/shopify/ShopifyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehacks.shopify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
