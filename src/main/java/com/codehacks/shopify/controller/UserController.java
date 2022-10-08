c24a6cdf15cf4349107bba50ee8eff125baa9bc5
Rume7 <electengrhume@gmail.com>
2022-10-08 09:31:36 +0100

    Created the user controller

---
+++ src/main/java/com/codehacks/shopify/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.codehacks.shopify.controller;
+
+import com.codehacks.shopify.model.User;
+import com.codehacks.shopify.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Rhume Disi
+ */
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    public User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+}
