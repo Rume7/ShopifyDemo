6ba4a10964295ac5112c4d1eca1143ce22ec32f2
Rume7 <electengrhume@gmail.com>
2022-10-08 09:23:57 +0100

    Created the User repository.

--- src/main/java/com/codehacks/shopify/repository/UserRepository.java
+++ src/main/java/com/codehacks/shopify/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.codehacks.shopify.repository;public interface UserRepository {
+package com.codehacks.shopify.repository;
+
+import com.codehacks.shopify.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
