320e0382491459bceb8b21a51cc033a4346ff3a7
Rume7 <electengrhume@gmail.com>
2022-10-10 08:32:59 +0100

    Added get user by id fxn.

--- src/main/java/com/codehacks/shopify/controller/UserController.java
+++ src/main/java/com/codehacks/shopify/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.codehacks.shopify.controller;
 
+import com.codehacks.shopify.exception.UserNotFoundException;
 import com.codehacks.shopify.model.User;
 import com.codehacks.shopify.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +13,7 @@ import java.util.List;
  */
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -28,5 +27,11 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
