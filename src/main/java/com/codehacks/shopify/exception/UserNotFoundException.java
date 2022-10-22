b24a8f147e029768ef4428d0b0ee996d74546fa3
Rume7 <electengrhume@gmail.com>
2022-10-22 14:23:29 +0100

    Improved exception class handler.

--- src/main/java/com/codehacks/shopify/exception/UserNotFoundException.java
+++ src/main/java/com/codehacks/shopify/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.codehacks.shopify.exception;
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(Long id) {
-        super("Could not found the user with id " + id);
+        super("Could not find the user with id " + id);
     }
 }
