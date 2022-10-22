9a68f20e561f3edc1d0bd205878cd4123b7e9085
Rume7 <electengrhume@gmail.com>
2022-10-22 14:22:28 +0100

    Added the update and delete methods.

--- src/main/java/com/codehacks/shopify/controller/UserController.java
+++ src/main/java/com/codehacks/shopify/controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
  */
 
 @RestController
+@RequestMapping("/api/v1/app")
 @CrossOrigin("http://localhost:3000")
 public class UserController {
 
@@ -33,5 +34,25 @@ public class UserController {
     public User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + " has been deleted.";
     }
 }
