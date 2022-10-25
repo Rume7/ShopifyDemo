418b6fe81c05a5afa6e038ab1d511e4d3c6003e8
Rume7 <electengrhume@gmail.com>
2022-10-25 23:23:20 +0100

    Add link to edit and view user.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Button } from "bootstrap";
 
 export default function Home() {
 
     const [users, setUsers] = useState([]);
+
+    const {id} =useParams()
 
     useEffect(() => {
         loadUsers();
@@ -14,6 +17,11 @@ export default function Home() {
         const result = await axios.get("http://localhost:8080/api/v1/app/users");
         setUsers(result.data);
     };
+
+    const deleteUser=async (id)=> {
+        await axios.delete(`http://localhost:8080/api/v1/app/user/${id}`)
+        loadUsers()
+    }
 
     return (
         <div className='container'>
@@ -37,9 +45,15 @@ export default function Home() {
                                     <td>{user.username}</td>
                                     <td>{user.email}</td>
                                     <td>
-                                        <button className="btn btn-primary mx-2">View</button>
-                                        <Link className="btn btn-outline-primary mx-2">Edit</Link>
-                                        <button className="btn btn-danger mx-2">Delete</button>
+                                        <Link className="btn btn-primary mx-2"
+                                            to={`/viewuser/${user.id}`}
+                                        >View</Link>
+                                        <Link className="btn btn-outline-primary mx-2"
+                                            to = {`/edituser/${user.id}`}
+                                        >Edit</Link>
+                                        <button className="btn btn-danger mx-2"
+                                            onClick={() => deleteUser(user.id)}
+                                        >Delete</button>
                                     </td>
                                 </tr>
                             ))
